efaf6e22a54890c614796c002018016da545383b
Hailey Gaylor <haileygaylor@gmail.com>
2019-10-14 21:33:13 -0700

    added view order logic to controller

--- Pierre-Bakery/Controllers/VendorOrderController.cs
+++ Pierre-Bakery/Controllers/VendorOrderController.cs
@@ -7,20 +7,35 @@ namespace VendorOrder.Controller
 {
     public class VendorOrderController : Controller
     {
-        [HttpGet("/vendororder/new")]
+        [HttpGet("/vendor/new")]
         public ActionResult New()
         {
             return View();
         }
 
-        [HttpPost("/vendororder")]
+        [HttpPost("/vendorlist")]
         public ActionResult Index(string vendorName, string vendorDiscription)
         {
             Vendor newvendor = new Vendor(vendorName, vendorDiscription);
             List<Vendor> vendorOrder = newvendor.VendorOrder;
 
 
+            Order newOrder = new Order(orderTitle, orderDiscription, price);
+            List<Order> Orders = newOrder.Orders;
+
             return View("Index", vendorOrder);
+        }
+
+        [HttpPost("/vendororders")]
+        public ActionResult Index(string orderTitle, string orderDiscription, int price)
+        {
+
+            Order newOrder = new Order(orderTitle, orderDiscription, price);
+            List<Order> Orders = newOrder.Orders;
+
+
+             return View("Index", Orders);
+
         }
 
     
